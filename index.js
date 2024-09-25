5df9af4d6f5407b5aade4da37dfdb25901beb661
Nandeesh241 <hnandeesh123@gmail.com>
2024-09-25 14:53:17 +0530

    its me

---
+++ index.js
@@ -0,0 +1,11 @@
+function vote ()
+{
+var name = document.getElementById ("Enter the name").value;
+var pass = 100*Math.random();
+var result = document.getElementById("result");
+result.innerHTML = name + "You have Scored"+pass.tofixed 2 + "%";
+if(passed > 35)
+    alert("FAIL");
+if (paased < 35 )
+    alert("Passed");
+}
